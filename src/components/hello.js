f36a22bb920f9a7c5c817afc278979eabcd261d1
vai na web <vainawebcpx@gmail.com>
2019-11-25 17:41:35 -0300

    brenno

---
+++ src/components/hello.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lampada: 'LAMPADA',
+        }
+    }
+
+    apagar = () => {
+        this.setState({
+            lampada: 'apagar',
+        });
+    }
+    acender = () => {
+        this.setState({
+            lampada: 'acender',
+        });
+    }
+    render() {
+        return(
+            <div>
+                <h1 className='ola'>
+                {this.state.lampada}
+                </h1>
+                <span>
+                    <button onClick={this.apagar}>apagar</button>
+                    <button onClick={this.acender}>acender</button>
+                </span>
+            </div>
+        )
+    }
+}
+
+export default Hello;
